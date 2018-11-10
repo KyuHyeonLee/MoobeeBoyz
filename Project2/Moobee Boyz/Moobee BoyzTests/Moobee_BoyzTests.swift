d8e640c9126f1824690926e5fae50ee6bee5b6d0
Lee Kyu Hyeon <44624908+KyuHyeonLee@users.noreply.github.com>
2018-11-09 20:35:38 -0500

    Add files via upload

---
+++ Project2/Moobee Boyz/Moobee BoyzTests/Moobee_BoyzTests.swift
@@ -0,0 +1,34 @@
+//
+//  Moobee_BoyzTests.swift
+//  Moobee BoyzTests
+//
+//  Created by COMP401 on 11/9/18.
+//  Copyright © 2018 COMP401. All rights reserved.
+//
+
+import XCTest
+@testable import Moobee_Boyz
+
+class Moobee_BoyzTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
